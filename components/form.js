5099d898810d6302b3526e0db6f2835f47ae4394
drili <DriliB@hotmail.com>
2022-07-09 16:46:23 +0200

    :sparkles: Init e#7

--- components/form.js
+++ components/form.js
@@ -1,67 +1,13 @@
-import { useReducer } from 'react'
-import { BiPlus } from 'react-icons/bi'
-import Success from './success'
-import Bug from './bug'
-
-const formReducer = (state, event) => {
-    return {
-        ...state,
-        [event.target.name]: event.target.value
-    }
-}
+import AddUserForm from "./addUserForm";
+import UpdateUserForm from "./updateUserForm";
 
 export default function Form() {
 
-    const [formData, setFormData] = useReducer(formReducer, {})
-
-    const handleSubmit = (e) => {
-        e.preventDefault()
-
-        if (Object.keys(formData).length == 0) {
-            return (
-                console.log("::: No form data found.")
-            )
-        }
-        console.log(formData)
-    }
-
-    if (Object.keys(formData).length > 0) {
-        return (
-            <Success message={"Data added"}></Success>
-        )
-    }
+    const flag = true
 
     return (
-        <form className="grid lg:grid-cols-2 w-4/6 gap-4" onSubmit={handleSubmit}>
-            <div className="input-type">
-                <input onChange={setFormData} className="border w-full px-5 py-3 focus:outline-none rounded-md" type="text" name="firstname" placeholder="First Name" />
-            </div>
-            <div className="input-type">
-                <input onChange={setFormData} className="border w-full px-5 py-3 focus:outline-none rounded-md" type="text" name="lastname" placeholder="Last Name" />
-            </div>
-            <div className="input-type">
-                <input onChange={setFormData} className="border w-full px-5 py-3 focus:outline-none rounded-md" type="text" name="email" placeholder="Email" />
-            </div>
-            <div className="input-type">
-                <input onChange={setFormData} className="border w-full px-5 py-3 focus:outline-none rounded-md" type="text" name="salary" placeholder="Salary" />
-            </div>
-            <div className="input-type">
-                <input onChange={setFormData} className="border px-5 py-3 focus:outline-none rounded-md" type="date" name="date"/>
-            </div>
-
-            <div className="flex gap-10 items-center">
-                <div className="form-check">
-                    <input onChange={setFormData} className="form-check-input appearance-none rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-green-500 checked:border-green-500 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-content float-left mr-2 cursor-pointer" value="Active" id="radioDefault1" type="radio" name="status"/>
-                    <label htmlFor="radioDefault1" className="inline-block text-gray-800">Active</label>
-                </div>
-
-                <div className="form-check">
-                    <input onChange={setFormData} className="form-check-input appearance-none rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-green-500 checked:border-green-500 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-content float-left mr-2 cursor-pointer" value="Inactive" id="radioDefault2" type="radio" name="status"/>
-                    <label htmlFor="radioDefault2" className="inline-block text-gray-800">Inactive</label>
-                </div>
-            </div>
-
-            <button className="flex justify-center text-md w-2/6 bg-green-500 text-white px-4 py-2 border rounded-md hover:bg-gray-50 hover:border-green-500 hover:text-green-500">Add <span className="px-1"><BiPlus size={24}></BiPlus></span></button>
-        </form>
+        <div className="container mx-auto py-5">
+            { flag ? <AddUserForm></AddUserForm> : <UpdateUserForm></UpdateUserForm> }
+        </div>
     )
 }
