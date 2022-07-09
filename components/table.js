dcffe626a997a9a7ffcd0fb0244abd5c275754ec
drili <DriliB@hotmail.com>
2022-07-09 17:00:53 +0200

    :rocket: Init e#8

--- components/table.js
+++ components/table.js
@@ -1,4 +1,5 @@
 import { BiEdit, BiTrashAlt } from 'react-icons/bi'
+import data from '../database/data.json'
 
 export default function Table() {
     return (
@@ -15,29 +16,40 @@ export default function Table() {
             </thead>
 
             <tbody className="bg-gray-200">
-                <tr className="bg-gray-50 text-center">
-                    <td className="px-16 py-2 flex flex-row items-center">
-                        <img src="#" alt="" />
-                        <span className="text-center ml-2 font-semibold">:::user_name</span>
-                    </td>
-                    <td className="px-16 py-2">
-                        <span>:::user_email</span>
-                    </td>
-                    <td className="px-16 py-2">
-                        <span>:::salary</span>
-                    </td>
-                    <td className="px-16 py-2">
-                        <span>:::birthday</span>
-                    </td>
-                    <td className="px-16 py-2">
-                        <button className="cursor"><span className="bg-green-500 text-white px-5 py-1 rounded-full">Active</span></button>
-                    </td>
-                    <td className="px-16 py-2 flex justify-around gap-5">
-                        <button className="cursor"><BiEdit size={25} color={"rgb(34, 197, 94)"}></BiEdit></button>
-                        <button className="cursor"><BiTrashAlt size={25} color={"rgb(244, 63, 94)"}></BiTrashAlt></button>
-                    </td>
-                </tr>
+                {
+                    data.map((obj, i) => (
+                        <TableRow {...obj} key={i}></TableRow>
+                    ))
+                }
+                {/* {TableRow()} */}
             </tbody>
         </table>
+    )
+}
+
+function TableRow({ id, name, avatar, email, salary, date, status }) {
+    return (
+        <tr className="bg-gray-50 text-center">
+            <td className="px-16 py-2 flex flex-row items-center">
+                <img src={avatar || "#"} alt="" />
+                <span className="text-center ml-2 font-semibold">{name || "Unknown name"}</span>
+            </td>
+            <td className="px-16 py-2">
+                <span>{email || "Unknown email"}</span>
+            </td>
+            <td className="px-16 py-2">
+                <span>{salary || "Unknown salary"}</span>
+            </td>
+            <td className="px-16 py-2">
+                <span>{date || "Unknown date"}</span>
+            </td>
+            <td className="px-16 py-2">
+                <button className="cursor"><span className="bg-green-500 text-white px-5 py-1 rounded-full">{status || "Unknown status"}</span></button>
+            </td>
+            <td className="px-16 py-2 flex justify-around gap-5">
+                <button className="cursor"><BiEdit size={25} color={"rgb(34, 197, 94)"}></BiEdit></button>
+                <button className="cursor"><BiTrashAlt size={25} color={"rgb(244, 63, 94)"}></BiTrashAlt></button>
+            </td>
+        </tr>
     )
 }
